22bcaac89a8e919ec0cb8ef25322af7d823ece8a
sumit s <sumitevs@gmail.com>
2023-09-20 23:13:46 +0530

    about me page

---
+++ Client/Pages/Team.Razor.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp.Client.Pages
+{
+    public partial class Team
+    {
+        IEnumerable<TeamMate> teamMates;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            teamMates = new List<TeamMate>();
+        }
+    }
+}
